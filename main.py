085b37cf35582e76563e30fad569b31337c946d4
marble0117 <guitarmarble0117@gmail.com>
2019-10-25 18:56:21 +0900

    add SGC

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from models.gcn import create_gcn_model
 from models.gat import create_gat_model
 from models.masked_gcn import create_masked_gcn_model
+from models.sgc import create_sgc_model
 from train import run
 
 if __name__ == '__main__':
@@ -12,6 +13,10 @@ if __name__ == '__main__':
     # dataset, model, optimizer = create_gat_model('Cora')
     # run(dataset, model, optimizer, patience=100)
 
+    # SGC
+    dataset, model, optimizer = create_sgc_model('Cora')
+    run(dataset, model, optimizer, epochs=100, early_stopping=False, verbose=False)
+
     # Masked GCN
-    dataset, model, optimizer = create_masked_gcn_model('Cora')
-    run(dataset, model, optimizer, verbose=True)
+    # dataset, model, optimizer = create_masked_gcn_model('Cora')
+    # run(dataset, model, optimizer, verbose=True)
