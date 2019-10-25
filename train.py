210ccdf47da9af462f52d26cf025ede54fb9ee0e
marble0117 <guitarmarble0117@gmail.com>
2019-10-25 13:03:52 +0900

    implement evaluate function

--- train.py
+++ train.py
@@ -14,6 +14,24 @@ def train(model, optimizer, data):
 def evaluate(model, data):
     model.eval()
 
+    with torch.no_grad():
+        output = model(data)
 
-def run(model, dataset):
-    optimizer = torch.optim.Adam()
+    outputs = {}
+    for key in ['train', 'val', 'test']:
+        mask = data['{}_mask'.format(key)]
+        loss = F.nll_loss(output[mask == 1], data.y[mask == 1]).item()
+        pred = output[mask == 1].max(dim=1)[1]
+        acc = pred.eq(data.y[mask == 1]).sum().item() / mask.sum().item()
+
+        outputs['{}_loss'.format(key)] = loss
+        outputs['{}_acc'.format(key)] = acc
+
+    return outputs
+
+
+def run(model, data, optimizer, epochs=200, early_stopping=True, patience=10):
+    for epoch in range(1, epochs+1):
+        train(model, optimizer, data)
+        evaluate(model, data)
+
