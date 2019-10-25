cae5a3768ca080ec694a11a54b5cfaca690daf8f
marble0117 <guitarmarble0117@gmail.com>
2019-10-25 16:04:26 +0900

    update GAT model and implement creating model function

--- gat.py
+++ gat.py
@@ -1,13 +1,18 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.optim import Adam
 from torch_geometric.nn import GATConv
+
+from datasets import get_planetoid_dataset
 
 
 class GAT(nn.Module):
-    def __init__(self, dataset, nhid, heads, dropout):
+    def __init__(self, dataset, nhid, first_heads, output_heads, dropout):
         super(GAT, self).__init__()
-        self.gc1 = GATConv(dataset.num_features, nhid, heads=heads, dropout=dropout)
-        self.gc2 = GATConv(nhid, dataset.num_classes, heads=1, dropout=dropout)
+        self.gc1 = GATConv(dataset.num_features, nhid,
+                           heads=first_heads, dropout=dropout)
+        self.gc2 = GATConv(nhid*first_heads, dataset.num_classes,
+                           heads=output_heads, dropout=dropout)
         self.dropout = dropout
 
     def forward(self, data):
@@ -18,3 +23,12 @@ class GAT(nn.Module):
         x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.gc2(x, edge_index)
         return F.log_softmax(x, dim=1)
+
+
+def create_gat_model(data_name, nhid=8, first_heads=8, output_heads=1,
+                     dropout=0.6, lr=0.005, weight_decay=5e-4):
+    dataset = get_planetoid_dataset(data_name, True)
+    model = GAT(dataset, nhid, first_heads, output_heads, dropout)
+    optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+
+    return dataset, model, optimizer
