aa452751f616c53a7b78bc4fc93e0b04b3622331
marble0117 <guitarmarble0117@gmail.com>
2019-10-25 18:28:49 +0900

    Masked GCN

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from models.gat import create_gat_model
 from models.gcn import create_gcn_model
+from models.gat import create_gat_model
+from models.masked_gcn import create_masked_gcn_model
 from train import run
 
 if __name__ == '__main__':
     # GCN
     # dataset, model, optimizer = create_gcn_model('Cora')
+    # run(dataset, model, optimizer, verbose=True)
 
     # GAT
-    dataset, model, optimizer = create_gat_model('Cora')
-    run(dataset, model, optimizer, patience=100)
+    # dataset, model, optimizer = create_gat_model('Cora')
+    # run(dataset, model, optimizer, patience=100)
+
+    # Masked GCN
+    dataset, model, optimizer = create_masked_gcn_model('Cora')
+    run(dataset, model, optimizer, verbose=True)
